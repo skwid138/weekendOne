4241c611cadca9716a005edb8b61b391f094e416
Hunter Rancourt <skwid138@gmail.com>
2017-09-02 20:42:04 -0500

    setup basic html file, sourced js and jQuery, and commented instructions into js

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('sourced -> script.js');
+
+/*Create an application that records employee salaries
+and adds salaries up to report monthly costs. */
+
+// The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
+
+//A 'Submit' button should collect the form information
+// store the information to calculate monthly costs
+// append information to the DOM
+// clear the input fields
+
+// Using the stored information
+// calculate monthly costs
+// append this to the to DOM
+
+// Hard Mode -- Create a delete button
+// Hard Mode -- that removes an employee from the DOM
+// Hard Mode -- it does not need to remove that Employee's salary from the reported total
+
+// Pro Mode -- Once the employee is deleted, update the total spend on salaries account for this employee's removal.
+// This will require that the logic knows which element was removed. 
+// Look into jQuery's .data() function. 
+// You will need to do something both when the employee is added
+// and when they are deleted.
