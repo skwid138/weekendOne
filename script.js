1f54385fa7747126f1802120b0a261ea0e8e7839
Hunter Rancourt <skwid138@gmail.com>
2017-09-03 17:09:00 -0500

    created constructor and declared variables for input

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 console.log('sourced -> script.js');
+var logTest = true;
 
 /*Create an application that records employee salaries
 and adds salaries up to report monthly costs. */
 
 // The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
 
+function onReady( ) {
+    console.log('JQ Ready!')
+    $('main').on('click', '#submit', submit)
 //A 'Submit' button should collect the form information
+}
+
+function submit( ) {
+    console.log('submit called');
+    
 // store the information to calculate monthly costs
+    var $firstName = $('#firstName');
+    var $lastName = $('#lastName');
+    var $idNumber = $('idNumber');
+    var $jobTitle = $('jobTitle');
+    var $annualSalary = $('#annualSalary');
+    if (logTest) console.log('$firstName ->', $firstName);
+    if (logTest) console.log('$lastName ->', $lastName);
+    if (logTest) console.log('$idNumber ->', $idNumber);
+    if (logTest) console.log('$jobTitle ->', $jobTitle);
+    if (logTest) console.log('$annualSalary ->', $annualSalary);
+    
+    
 // append information to the DOM
 // clear the input fields
+}
+
+// builds objects with employee info
+function Employee ( firstName, lastName, idNumber, jobTitle, annualSalary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = idNumber;
+    this.jobTitle = jobTitle;
+    this.annualSalary = annualSalary;
+}
 
 // Using the stored information
 // calculate monthly costs
@@ -23,3 +54,5 @@ and adds salaries up to report monthly costs. */
 // Look into jQuery's .data() function. 
 // You will need to do something both when the employee is added
 // and when they are deleted.
+
+$(document).ready(onReady);
