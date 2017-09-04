3bd56740d24f4ce322feafa57376abfb7bd8faf3
Hunter Rancourt <skwid138@gmail.com>
2017-09-04 13:30:33 -0500

    Hard Mode completed

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var logTest = true;
 and adds salaries up to report monthly costs. */
 
 function onReady( ) {
-    console.log('JQ Ready!')
+    console.log('JQ Ready!');
     //A 'Submit' button should collect the form information
-    $('main').on('click', '#submit', submit)
+    $('main').on('click', '#submit', submit);
+    // Hard Mode -- Create a delete button
+    $('main').on('click', '.removeButton', deleteRecord);
 }
 
 // global array of employee objects
@@ -29,16 +31,19 @@ function submit( ) {
     if (logTest) console.log('$jobTitle ->', $jobTitle);
     if (logTest) console.log('$annualSalary ->', $annualSalary);
 
-    new Employee($firstName, $lastName, $idNumber, $jobTitle, $annualSalary)
+    new Employee($firstName, $lastName, $idNumber, $jobTitle, $annualSalary);
     
-    var $employeeAppend = $('tbody:last-child');
+    var $employeeAppend = $('table > tbody:last-child');
     // append information to the DOM
-    $employeeAppend.append("<tr>");
-    $employeeAppend.append("<td><span>" + $firstName + "</span></td>");
-    $employeeAppend.append("<td><span>" + $lastName + "</span></td>");
-    $employeeAppend.append("<td><span>" + $idNumber + "</span></td>");
-    $employeeAppend.append("<td><span>" + $jobTitle + "</span></td>");
-    $employeeAppend.append("<td><span>" + $annualSalary + "</span></td>");
+    // $employeeAppend.append("<tr>");
+    // using the below mess my <td>'s are actually being put inside the <tr> which makes my remove button work
+    $employeeAppend.append("<tr><td><span>" + $firstName + "</span></td><td><span>" + $lastName + "</span></td><td><span>" + $idNumber + "</span></td><td><span>" + $jobTitle + "</span></td><td><span>" + $annualSalary + "</span></td><td><button class='removeButton'>Remove</button></td>");
+    // $employeeAppend.append("<td><span>" + $lastName + "</span></td>");
+    // $employeeAppend.append("<td><span>" + $idNumber + "</span></td>");
+    // $employeeAppend.append("<td><span>" + $jobTitle + "</span></td>");
+    // $employeeAppend.append("<td><span>" + $annualSalary + "</span></td>");
+    // Hard Mode -- Create a delete button
+    // $employeeAppend.append("<td><button class='removeButton'>Remove</button></td>");
 
     // clear the input fields
     $('#firstName').val( '' );
@@ -47,8 +52,8 @@ function submit( ) {
     $('#jobTitle').val( '' );
     $('#annualSalary').val( '' );
 
+    // append this to the to DOM
     var $calcSalariesOutput = $('#calcSalariesOutput');
-
     $calcSalariesOutput.text(calcSalaries(employeeList));
 }
 
@@ -70,7 +75,6 @@ function Employee ( firstName, lastName, idNumber, jobTitle, annualSalary) {
 // looks at an array of employees and calculates the monthly cost
 function calcSalaries( employeeList ) {
     // Using the stored information calculate monthly costs
-    // var for total salary
     var totalSalary = 0;
     // loop through array
     for (var i = 0; i < employeeList.length; i++) {
@@ -79,18 +83,22 @@ function calcSalaries( employeeList ) {
     var monthlyCost = totalSalary / 12;
     return monthlyCost;
 }
-// append this to the to DOM
 
-
-
-// Hard Mode -- Create a delete button
 // Hard Mode -- that removes an employee from the DOM
 // Hard Mode -- it does not need to remove that Employee's salary from the reported total
+function deleteRecord ( ) {
+    if (logTest) console.log('deleteRecord called');
+    // looks like these both target the same element
+    if (logTest) console.log($(this).closest('tr'));
+    if(logTest) console.log($(this).parents('tr:first'));
+    $(this).closest('tr').remove();
 
-// Pro Mode -- Once the employee is deleted, update the total spend on salaries account for this employee's removal.
-// This will require that the logic knows which element was removed. 
-// Look into jQuery's .data() function. 
-// You will need to do something both when the employee is added
-// and when they are deleted.
+    // Pro Mode -- Once the employee is deleted, update the total spend on salaries account for this employee's removal.
+    // This will require that the logic knows which element was removed. 
+    // Look into jQuery's .data() function. 
+    // You will need to do something both when the employee is added
+    // and when they are deleted.
+   
+}
 
 $(document).ready(onReady);
